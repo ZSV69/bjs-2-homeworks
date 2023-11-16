7c18d694379ea6746a7f0176f19180de25038cdd
Zvyagintsev Sergey <zvyagintsevsergey@yandex.ru>
2023-11-17 00:25:45 +0300

    task 1 ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+
+    let arr = []
+    let discriminant = b** 2 - 4 * a * c;
+
+    if (discriminant < 0) {
+        return arr;
+}   else if (discriminant === 0) {
+        const root = -b / (2 * a);
+        arr.push (root);
+    } else {
+        const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        arr.push (root1,root2);
+    }
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+      percent = percent/100/12;
+
+      let creditBody = amount - contribution;
+      let monthlyPayment = creditBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+      let totalPayment = monthlyPayment * countMonths;
+
+      totalPayment = Number(totalPayment.toFixed(2));
+
+      return totalPayment;
 }
