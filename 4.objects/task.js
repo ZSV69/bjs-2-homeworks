57cc4f31cc47ede9376f801077a4f446180f14ba
Zvyagintsev Sergey <zvyagintsevsergey@yandex.ru>
2023-12-21 23:29:56 +0300

    ready HW object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
 function Student(name, gender, age) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+    this.setSubject = function(subjectName){
+        this.subject = subjectName;
+    }
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+    this.addMarks = function (...marksToAdd) {
+        if (!this.marks) {
+            console.log ("Error: Student has been expelled.");
+            return;
+        }
+        this.marks.push (...marksToAdd);
+    }
 
-Student.prototype.getAverage = function () {
-  
-}
+    this.getAverage = function() {
+        if (!this.marks || !this.marks.length) {
+            return 0;
+        }
+        const sum = this.marks.reduce((acc,curr)=> acc + curr);
+        return sum / this.marks.length;
+    }
 
-Student.prototype.exclude = function (reason) {
-  
+    this.exclude = function (reason) {
+        delete this.subject;
+        delete this.marks;
+        this.excluded = reason;
+    }
 }
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
